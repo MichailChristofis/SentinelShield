f36470be596c1d036718eba55a0b7d88da4f9985
s3923089 <90187172+MichailChristofis@users.noreply.github.com>
2024-03-28 18:40:11 +0200

    did 3.2, 3.3 and 3.4
    Create Main class and ensure program is starting correctly. Add properties to the main class to keep track of a user and ticket list. Create 'User' class with correct attributes, getters, and setters.

---
+++ SentinelShield.java
@@ -0,0 +1,46 @@
+//SentinelShield is the main class of the project.
+//It handles all communication with the user, and
+//uses the data garnered from that communication
+//appropriately, sending it to the relevant department
+//and people
+public class SentinelShield {
+	//The loginScreen() method, handles the user interface
+	//and communication with the user, for the login screen.
+	private void loginScreen() {
+		
+	}
+	//The forgotPasswordScreen() method, handles the user interface
+	//and communication with the user, for the forgot password screen.
+	private void forgotPasswordScreen() {
+		
+	}
+	//The signupScreen() method, handles the user interface
+	//and communication with the user, for the signup screen.
+	private void signupScreen() {
+		
+	}
+	//The signupScreen() method, handles the user interface
+	//and communication with the user, for the signup screen.
+	private void viewTicketsScreen() {
+		
+	}
+	//The createTicketScreen() method, handles the user interface
+	//and communication with the user, for the create ticket screen.
+	private void createTicketScreen() {
+		
+	}
+	//The completeTicketScreen() method, handles the user interface
+	//and communication with the user, for the complete ticket screen.
+	private void completeTicketScreen() {
+		
+	}
+	//The checkForArchivedTickets() method, ....
+	//Joshua all yours mate, not sure what you wanted here
+	private void checkForArchivedTickets() {
+		
+	}
+	public static void main(String[] args) {
+		System.out.println("Welcome to SentinelShield!");
+		
+	}
+}
---
+++ Ticket.java
@@ -0,0 +1,69 @@
+import java.time.LocalDate;
+
+//The Ticket class is used to store a ticket's severity,
+//status, description, and the date of its completion.
+public class Ticket {
+	enum Severity{
+		Low,
+		Medium,
+		High
+	}
+	enum TicketStatus{
+		Open,
+		CompletedResolved,
+		CompletedUnresolved,
+		Archived
+	}
+	private String description;
+	private Severity severity;
+	private TicketStatus ticketStatus;
+	private LocalDate dateCompleted;
+	
+	//The Ticket() method is a constructor for the Ticket class.
+	public Ticket(String description, Severity severity, TicketStatus ticketStatus, LocalDate dateCompleted) {
+		this.description=description;
+		this.severity=severity;
+		this.ticketStatus=ticketStatus;
+		this.dateCompleted=dateCompleted;
+	}
+	//The getDescription() method is a getter method,
+	//for the ticket's description.
+	public String getDescription() {
+		return this.description;
+	}
+	//The setDescription() method is a setter method,
+	//for the ticket's description.
+	public void setDescription(String description) {
+		this.description=description;
+	}
+	//The getSeverity() method is a getter method,
+	//for the ticket's severity.
+	public Severity getSeverity() {
+		return this.severity;
+	}
+	//The setSeverity() method is a setter method,
+	//for the ticket's severity.
+	public void setSeverity(Severity severity) {
+		this.severity=severity;
+	}
+	//The getTicketStatus() method is a getter method,
+	//for the ticket's status.
+	public TicketStatus getTicketStatus() {
+		return this.ticketStatus;
+	}
+	//The setTicketStatus() method is a setter method,
+	//for the ticket's status.
+	public void setTicketStatus(TicketStatus ticketStatus) {
+		this.ticketStatus=ticketStatus;
+	}
+	//The getDateCompleted() method is a getter method,
+	//for the ticket's date.
+	public LocalDate getDateCompleted() {
+		return this.dateCompleted;
+	}
+	//The setDateCompleted() method is a setter method,
+	//for the ticket's date.
+	public void setDateCompleted(LocalDate dateCompleted) {
+		this.dateCompleted=dateCompleted;
+	}
+}
