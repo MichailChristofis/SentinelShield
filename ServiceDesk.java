04451b57007f9677b1193e6c0a6d3ceff2a1c146
Joshua McCorkell <joshua@themccorkells.com>
2024-03-29 15:16:24 +1000

    Fix Formatting.

--- ServiceDesk.java
+++ ServiceDesk.java
@@ -3,28 +3,32 @@
 //as its criteria both severity, and number of tickets assigned
 //to each technician
 public class ServiceDesk {
-	private User techniciansLevel1[], techniciansLevel2[];
-	private int assignedTickets[];
-	//The ServiceDesk() method is the constructor for the
-	//ServiceDesk class.
-	public ServiceDesk() {
-		techniciansLevel1=new User[3];
-		techniciansLevel1[0]=new User("harrystyles@gmail.com", "Harry", "Styles", "(02) 1234 5678", "harryharry", true);
-		techniciansLevel1[1]=new User("niallhorran@gmail.com", "Niall", "Horan", "(02) 5678 1234", "nialnial", true);
-		techniciansLevel1[2]=new User("liampayne@gmail.com", "Liam", "Payne", "(02) 1234 5666", "limaliam", true);
-		techniciansLevel2=new User[2];
-		techniciansLevel2[0]=new User("louistomlison@gmail.com", "Louis", "Tomlison", "(02) 1234 1234", "louislouis", true);
-		techniciansLevel2[1]=new User("zaynmalik@gmail.com", "Zayn", "Malik", "(02) 5678 5678", "zaynzayn", true);
-		assignedTickets=new int[5];
-		for(int i=0; i<5; i++) {
-			assignedTickets[i]=0;
-		}
-	}
-	//The AssignTicket() method, assigns a ticket to the
-	//appropriate technician, based on its severity, and
-	//the number of tickets already assigned to each
-	//technician.
-	public void AssignTicket(Ticket toAssign) {
-		
-	}
+    private User techniciansLevel1[], techniciansLevel2[];
+    private int assignedTickets[];
+
+    // The ServiceDesk() method is the constructor for the
+    // ServiceDesk class.
+    public ServiceDesk() {
+        techniciansLevel1 = new User[3];
+        techniciansLevel1[0] = new User("harrystyles@gmail.com", "Harry", "Styles", "(02) 1234 5678", "harryharry",
+                true);
+        techniciansLevel1[1] = new User("niallhorran@gmail.com", "Niall", "Horan", "(02) 5678 1234", "nialnial", true);
+        techniciansLevel1[2] = new User("liampayne@gmail.com", "Liam", "Payne", "(02) 1234 5666", "limaliam", true);
+        techniciansLevel2 = new User[2];
+        techniciansLevel2[0] = new User("louistomlison@gmail.com", "Louis", "Tomlison", "(02) 1234 1234", "louislouis",
+                true);
+        techniciansLevel2[1] = new User("zaynmalik@gmail.com", "Zayn", "Malik", "(02) 5678 5678", "zaynzayn", true);
+        assignedTickets = new int[5];
+        for (int i = 0; i < 5; i++) {
+            assignedTickets[i] = 0;
+        }
+    }
+
+    // The AssignTicket() method, assigns a ticket to the
+    // appropriate technician, based on its severity, and
+    // the number of tickets already assigned to each
+    // technician.
+    public void AssignTicket(Ticket toAssign) {
+
+    }
 }
--- User.java
+++ User.java
@@ -2,76 +2,90 @@
 //their email, first name, last name, phone number, password
 //and whether or not they are a technician.
 public class User {
-	private String email, firstName, lastName, phoneNumber, password;
-	private boolean isTechnician;
-	//The User() method, is the constructor for the User class.
-	public User(String email, String firstName, String lastName, String phoneNumber, String password, boolean isTechnician) {
-		this.email=email;
-		this.firstName=firstName;
-		this.lastName=lastName;
-		this.phoneNumber=phoneNumber;
-		this.password=password;
-		this.isTechnician=isTechnician;
-	}
-	//The getEmail() method is a getter method,
-	//for the user's email.
-	public String getEmail() {
-		return this.email;
-	}
-	//The setEmail() method is a setter method,
-	//for the user's email.
-	public void setEmail(String email) {
-		this.email=email;
-	}
-	//The getFirstName() method is a getter method,
-	//for the user's first name.
-	public String getFirstName() {
-		return this.firstName;
-	}
-	//The setFirstName() method is a setter method,
-	//for the user's first name.
-	public void setFirstName(String firstName) {
-		this.firstName=firstName;
-	}
-	//The getLastName() method is a getter method,
-	//for the user's last name.
-	public String getLastName() {
-		return this.lastName;
-	}
-	//The setLastName() method is a setter method,
-	//for the user's last name.
-	public void setLastName(String lastName) {
-		this.lastName=lastName;
-	}
-	//The getPhoneNumber() method is a getter method,
-	//for the user's phone number.
-	public String getPhoneNumber() {
-		return this.phoneNumber;
-	}
-	//The setPhoneNumber() method is a setter method,
-	//for the user's phone number.
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber=phoneNumber;
-	}
-	//The getPassword() method is a getter method,
-	//for the user's email.
-	public String getPassword() {
-		return this.password;
-	}
-	//The setPassword() method is a setter method,
-	//for the user's email.
-	public void setPassword(String password) {
-		this.password=password;
-	}
-	//The getIsTechnician() method is a getter method,
-	//for whether the user is a technician.
-	public boolean getIsTechnician() {
-		return this.isTechnician;
-	}
-	//The setIsTechnician() method is a getter method,
-	//for whether the user is a technician.
-	public void setIsTechnician(boolean isTechnician) {
-		this.isTechnician=isTechnician;
-	}
-	
+    private String email, firstName, lastName, phoneNumber, password;
+    private boolean isTechnician;
+
+    // The User() method, is the constructor for the User class.
+    public User(String email, String firstName, String lastName, String phoneNumber, String password,
+            boolean isTechnician) {
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.password = password;
+        this.isTechnician = isTechnician;
+    }
+
+    // The getEmail() method is a getter method,
+    // for the user's email.
+    public String getEmail() {
+        return this.email;
+    }
+
+    // The setEmail() method is a setter method,
+    // for the user's email.
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    // The getFirstName() method is a getter method,
+    // for the user's first name.
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    // The setFirstName() method is a setter method,
+    // for the user's first name.
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    // The getLastName() method is a getter method,
+    // for the user's last name.
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    // The setLastName() method is a setter method,
+    // for the user's last name.
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    // The getPhoneNumber() method is a getter method,
+    // for the user's phone number.
+    public String getPhoneNumber() {
+        return this.phoneNumber;
+    }
+
+    // The setPhoneNumber() method is a setter method,
+    // for the user's phone number.
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    // The getPassword() method is a getter method,
+    // for the user's email.
+    public String getPassword() {
+        return this.password;
+    }
+
+    // The setPassword() method is a setter method,
+    // for the user's email.
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    // The getIsTechnician() method is a getter method,
+    // for whether the user is a technician.
+    public boolean getIsTechnician() {
+        return this.isTechnician;
+    }
+
+    // The setIsTechnician() method is a getter method,
+    // for whether the user is a technician.
+    public void setIsTechnician(boolean isTechnician) {
+        this.isTechnician = isTechnician;
+    }
+
 }
